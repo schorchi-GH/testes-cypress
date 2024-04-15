76ed1f3376b71117e22b036b4594bcf74d96928a
Schorchi <schorchi@gmail.com>
2024-04-15 16:19:00 -0300

    Manipulando Formulários (34.3)

---
+++ cypress/e2e/ebac-jobs/candidatura.cy.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+
+describe('Testes para a página de candidatura', () => {
+    beforeEach(() => {
+        cy.visit('https://ebac-jobs-e2e.vercel.app/')
+    })
+
+    it('Deve levar o usuário até o formulário de inscrição', () => {
+        cy.get('.Vaga_vagaLink__DeFkk').first().click()
+        cy.get('input').should('have.length', 7)
+    })
+
+    it('Deve preencher o formulário de inscrição', () => {
+        cy.get('.Vaga_vagaLink__DeFkk').first().click()
+        cy.get('input[name="nome-completo"]').type('georges hutschinski')
+        cy.get('input[name="email"]').type('georgeshutschinski@teste.com')
+        cy.get('input[name="telefone"]').type('11 98911-8110')
+        cy.get('input[name="endereco"]').type('rua jeste, bairro cypress, são paulo-sp')
+        cy.get('#linux').check()
+        cy.get('select[name="escolaridade"]').select('outros')
+        cy.get('.Aplicacao_button__tw2AE').click()
+
+        cy.on('window:alert', (conteudo) => {
+            expect(conteudo).contain('Obrigado pela candidatura!')
+        })
+    })
+})
--- cypress/e2e/ebac-jobs/home.cy.js
+++ cypress/e2e/ebac-jobs/home.cy.js
@@ -1,13 +1,16 @@
 /// <reference types="cypress" />
 
 describe('Testes para a home', () => {
-    it('Deve renderizar 4 vagas', () => {
+
+    beforeEach(() => {
         cy.visit('https://ebac-jobs-e2e.vercel.app/')
+    })
+
+    it('Deve renderizar 4 vagas', () => {
         cy.get('.ListaVagas_vagas__gmNZn > li').should('have.length', 4)
     })
 
     it('Deve filtrar por fullstack', () => {
-        cy.visit('https://ebac-jobs-e2e.vercel.app/')
         cy.get('.FormVagas_campo__E1ppF').type('fullstack')
         cy.get('button[type="submit"]').click()
         cy.get('.ListaVagas_vagas__gmNZn > li').should('have.length', 1)
